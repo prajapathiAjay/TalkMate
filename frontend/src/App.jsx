2a3caad4185582b473db88443898d73cb96ff0c5
prajapathiAjay <ajayworking6309@gmail.com>
2025-11-15 19:39:13 +0530

    socekt.io setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Chat from './pages/Chat.jsx'
+import Chat from "./pages/Chat.jsx"
+import socket from './Socket.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
+
+    useEffect(() => {
+    socket.connect(); // 🔥 connect ONCE when App mounts
+
+    return () => socket.disconnect(); // optional cleanup
+  }, []);
+//  socket.connect();
   return (
    <Chat/>
   )
